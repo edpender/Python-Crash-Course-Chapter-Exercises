92797666590bb119d9043cd3f2efdf70342330d3
Edward Pender <87315746+edpender@users.noreply.github.com>
2021-08-01 01:31:14 +0100

    Add files via upload

---
+++ Python_Chapter10_Exercises_Learning_C.py
@@ -0,0 +1,8 @@
+filename = 'learning_python.txt'
+
+with open(filename) as f:
+    lines = f.readlines()
+
+for line in lines:
+    # Get rid of newline, then replace Python with C.
+    print(line.rstrip().replace('Python', 'C'))
